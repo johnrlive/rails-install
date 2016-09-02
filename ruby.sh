acd17321669f1358ccb547fca6b19647101804e0
John R Live <john@addlocal.com>
2016-09-01 22:44:22 -0400

    Update ruby.sh

--- ruby.sh
+++ ruby.sh
@@ -38,13 +38,15 @@ echo '[###### Done ######]'
 sleep 1
 
 
+echo '[###### Install rbenv ######]'
+sleep 1
 cd ~ 
 git clone git://github.com/sstephenson/rbenv.git .rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+# echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+# echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
+# echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
 source ~/.bash_profile
 
 
